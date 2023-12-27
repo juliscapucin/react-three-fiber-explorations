07a1e48029cb406c4a6a6cf0e1adbee2f0c64fab
juliscapucin <jscapucin@gmail.com>
2023-12-27 12:38:15 +0100

    components: add transformControl

--- src/components/transformControl.tsx
+++ src/components/transformControl.tsx
@@ -1,3 +1,16 @@
+import { OrbitControls, TransformControls } from "@react-three/drei"
+
 export default function TransformControl() {
-	return <div>T</div>
+	return (
+		<>
+			{/* makeDefault isolates orbitControls from transformControls */}
+			<OrbitControls makeDefault />
+			<TransformControls>
+				<mesh>
+					<boxGeometry />
+					<meshBasicMaterial color='hotpink' />
+				</mesh>
+			</TransformControls>
+		</>
+	)
 }
